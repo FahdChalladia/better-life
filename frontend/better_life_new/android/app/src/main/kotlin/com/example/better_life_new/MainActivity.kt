147cd48dbea2f46063842e921ee08bdf169091ad
Fahd Challadia <139588511+FahdChalladia@users.noreply.github.com>
2026-03-18 03:25:13 +0100

    Delete frontend/better_life_new directory

--- frontend/better_life_new/android/app/src/main/kotlin/com/example/better_life_new/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.better_life_new
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/better_life_new/lib/utils/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const background = Color(0xFFEFEFEF);
-  static const input = Color(0xFFDDE87A);
-  static const button = Color(0xFFD32F2F);
-  static const textDark = Color(0xFF333333);
-}
