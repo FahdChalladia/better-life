decd2259fd4f01629697f1c7cb3f527f06ab667a
Fahd Challadia <139588511+FahdChalladia@users.noreply.github.com>
2026-03-26 16:30:19 +0100

    Delete backend/config.py

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    MONGO_URI = os.getenv(
-        "MONGO_URI",
-        "mongodb+srv://fahd:fahd2004@better-life.vq0esnz.mongodb.net/mood_tracker?retryWrites=true&w=majority"
-    )
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "supersecretkey")
